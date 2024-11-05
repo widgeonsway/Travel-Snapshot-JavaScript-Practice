8aa7f00e3230a2b9c2f11ee9b3fbe675157ae9f9
Widgeon <91209299+widgeonsway@users.noreply.github.com>
2024-11-04 18:55:49 -0700

    loopified

--- scripts.js
+++ scripts.js
@@ -1,179 +1,100 @@
 "use strict";
 
-const amsterdamFunction = function () {
-  title.textContent = "Amsterdam";
-  description.textContent = "This is Amsterdam";
-  document.getElementById("image").src = "destination1.png";
-  document.getElementById("image").alt =
-    "A picture of canal boats and a church";
-  document.body.classList.add("amsterdam");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-};
+const cityDestinations = [
+  {
+    name: "amsterdam",
+    title: "Amsterdam",
+    description: "This is Amsterdam",
+    src: "destination1.png",
+    alt: "A picture of canal boats and a church",
+  },
+  {
+    name: "bath",
+    title: "Bath",
+    description: "This is Bath",
+    src: "destination2.png",
+    alt: "A picture of the Roman Baths in Bath",
+  },
+  {
+    name: "london",
+    title: "London",
+    description: "This is Camden Town",
+    src: "destination3.png",
+    alt: "A picture of house boats on the river",
+  },
+  {
+    name: "antwerp",
+    title: "Antwerp",
+    description: "This is Antwerp",
+    src: "destination4.png",
+    alt: "A picture of Antwerp",
+  },
+  {
+    name: "brussels",
+    title: "Brussels",
+    description: "This is Brussels",
+    src: "destination5.png",
+    alt: "A picture of Brussels",
+  },
+  {
+    name: "paris",
+    title: "Paris",
+    description: "This is Paris",
+    src: "destination6.png",
+    alt: "A picture of Paris",
+  },
+  {
+    name: "bruges",
+    title: "Bruges",
+    description: "This is Bruges",
+    src: "destination7.png",
+    alt: "A picture of Bruges",
+  },
+  {
+    name: "ghent",
+    title: "Ghent",
+    description: "This is Ghent",
+    src: "destination8.png",
+    alt: "A picture of Ghent",
+  },
+  {
+    name: "kilkenny",
+    title: "Kilkenny",
+    description: "This is Kilkenny",
+    src: "destination9.png",
+    alt: "A picture of Kilkenny",
+  },
+];
 
-const bathFunction = function () {
-  title.textContent = "Bath";
-  description.textContent = "This is Bath";
-  document.getElementById("image").src = "destination2.png";
-  document.getElementById("image").alt = "A picture of the Roman baths in Bath";
-  document.body.classList.add("bath");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-};
+const city = cityDestinations.map((destination) => destination.title);
 
-const londonFunction = function () {
-  title.textContent = "London";
-  description.textContent = "This is Camden Town in London";
-  document.getElementById("image").src = "destination3.png";
-  document.getElementById("image").alt =
-    "A picture of a house boat on a tiny river";
-  document.body.classList.add("london");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-};
+cityDestinations.forEach(function (destination) {
+  const destinationFunction = function () {
+    title.textContent = destination.title;
+    description.textContent = destination.description;
+    document.getElementById("image").src = destination.src;
+    document.getElementById("image").alt = destination.alt;
+    document.body.classList.remove(...document.body.classList);
+    document.body.classList.add(destination.name);
+  };
 
-const antwerpFunction = function () {
-  title.textContent = "Antwerp";
-  description.textContent = "This is Antwerp";
-  document.getElementById("image").src = "destination4.png";
-  document.getElementById("image").alt =
-    "A picture of the old buildings in Antwerp";
-  document.body.classList.add("antwerp");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-};
-
-const brusselsFunction = function () {
-  title.textContent = "Brussels";
-  description.textContent = "This is Grand Place in Brussels";
-  document.getElementById("image").src = "destination5.png";
-  document.getElementById("image").alt =
-    "A picture of the buildings in Grand Place in Brussels";
-  document.body.classList.add("brussels");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-};
-
-const parisFunction = function () {
-  title.textContent = "Paris";
-  description.textContent = "This is the bottom of Sacre-Coeur in Paris ";
-  document.getElementById("image").src = "destination6.png";
-  document.getElementById("image").alt =
-    "A picture of a merry go round and the church behind it ";
-  document.body.classList.add("paris");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("ghent");
-};
-
-const brugesFunction = function () {
-  title.textContent = "Bruges";
-  description.textContent = "This is Bruges";
-  document.getElementById("image").src = "destination7.png";
-  document.getElementById("image").alt = "A picture of the skinniest church";
-  document.body.classList.add("bruges");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("ireland");
-  document.body.classList.remove("ghent");
-};
-
-const ghentFunction = function () {
-  title.textContent = "Ghent";
-  description.textContent = "This is ghent";
-  document.getElementById("image").src = "destination8.png";
-  document.getElementById("image").alt =
-    "A picture of the famous buildings in Ghent next to the water";
-  document.body.classList.add("ghent");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("antwerp");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ireland");
-};
-
-const irelandFunction = function () {
-  title.textContent = "Ireland";
-  description.textContent = "This is the river and castle in Kilkenny";
-  document.getElementById("image").src = "destination9.png";
-  document.getElementById("image").alt =
-    "A picture of the side of the Kilkenny castle next to the water with a swan ";
-  document.body.classList.add("ireland");
-  document.body.classList.remove("amsterdam");
-  document.body.classList.remove("bath");
-  document.body.classList.remove("london");
-  document.body.classList.remove("brussels");
-  document.body.classList.remove("paris");
-  document.body.classList.remove("bruges");
-  document.body.classList.remove("ghent");
-  document.body.classList.remove("antwerp");
-};
-
+  document
+    .getElementById(destination.name)
+    .addEventListener("click", destinationFunction);
+});
+//remember to ask if 71-18 and 88-99 can be done the same
 const btnRandom = document.querySelector("#btn-random-destination");
 
 btnRandom.addEventListener("click", function () {
-  //Generate a random dice roll
-  const destination = Math.trunc(Math.random() * 9) + 1;
-  //display dice
-  if (destination === 1) amsterdamFunction();
-  else if (destination === 2) bathFunction();
-  else if (destination === 3) londonFunction();
-  else if (destination === 4) antwerpFunction();
-  else if (destination === 5) brusselsFunction();
-  else if (destination === 6) parisFunction();
-  else if (destination === 7) brugesFunction();
-  else if (destination === 8) ghentFunction();
-  else if (destination === 9) irelandFunction();
-});
+  const randomDestination = Math.trunc(Math.random() * cityDestinations.length);
+  const randomMatch = cityDestinations[randomDestination];
 
-amsterdam.addEventListener("click", amsterdamFunction);
-bath.addEventListener("click", bathFunction);
-london.addEventListener("click", londonFunction);
-antwerp.addEventListener("click", antwerpFunction);
-brussels.addEventListener("click", brusselsFunction);
-paris.addEventListener("click", parisFunction);
-bruges.addEventListener("click", brugesFunction);
-ghent.addEventListener("click", ghentFunction);
-ireland.addEventListener("click", irelandFunction);
+  title.textContent = randomMatch.title;
+  description.textContent = randomMatch.description;
+  document.getElementById("image").src = randomMatch.src;
+  document.getElementById("image").alt = randomMatch.alt;
+
+  document.body.classList.remove(...document.body.classList);
+
+  document.body.classList.add(randomMatch.name);
+});
